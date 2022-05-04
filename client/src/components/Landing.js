b61b0cc1c1d9c429191fa5e20d7f0a57f4f54d9a
Sam <sahasamarpan01@gmail.com>
2022-05-04 21:24:39 +0530

    Landing page includes dashboard navigation

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,22 @@
-const Landing=()=>{
-    return(
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <div>
+      <div className="container" style={{ alignItems: "center" }}>
         <div>
             Landing
         </div>
-    );
+        <Link
+          to="/surveys"
+          className="waves-effect waves-light btn-large"
+          style={{ alignItems: "center" }}
+        >
+          Go to Dashboard
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
